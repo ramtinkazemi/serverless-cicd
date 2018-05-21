64b4449edf84bdf8e1e61f849c594b2df15929a6
Jason Lau <lxgzmy@gmail.com>
2018-05-21 22:42:36 +1000

    second change 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
 
-    console.log("another change");
+    console.log("another change 1");
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
