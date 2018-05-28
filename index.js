469bd81f2736f0dc556442d007d4d8cd459ce975
Jason Liu <lxgzmy@gmail.com>
2018-05-28 17:24:50 +1000

    version 8

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ exports.handler = (event, context, callback) => {
     console.log("another change 2");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is 333: version 7' ,
+        body: 'The time in Los Angeles is 333: version 8' ,
     });
 };
