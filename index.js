d3ce37efa66a8e4f3f489a5404d6878213e1e430
Ramtin Kazemi <ramtin.kazemi@cashrewards.com>
2018-05-30 15:48:46 +1000

    V 10

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ exports.handler = (event, context, callback) => {
     console.log("another change 2");
     callback(null, {
         statusCode: '200',
-        body: 'Version 9' ,
+        body: 'Version 10' ,
     });
 };
