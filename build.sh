f139db9824aa0ebb7ea608f8cf8f2a7e4c579f5e
Ramtin Kazemi <ramtin.kazemi@cashrewards.com>
2018-06-04 12:34:23 +1000

    fixed S3 paths

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm outputSamTemplate.yaml
-aws cloudformation package --template-file samTemplate.yaml --s3-bucket cashrewards.dev.ramtin-test-slscicd --output-template-file outputSamTemplate.yaml
+aws cloudformation package --template-file samTemplate.yaml --s3-bucket cashrewards.dev.bitbucket --s3-prefix /ShopGoDev/serverless-cicd/master/deploy --output-template-file outputSamTemplate.yaml
 #aws cloudformation deploy --template-file outputSamTemplate.yaml --stack-name mycicdstack  --capabilities CAPABILITY_IAM
